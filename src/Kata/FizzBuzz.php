14f442efacc2d9d7d77effe30f90fc59887b3cc3
danielescillia <daniele.scillia@gmail.com>
2020-11-01 23:18:34 +0100

    solved testItReturnsBuzzWhenReceivingNumber20 with OBVIOUS IMPLEMENTATION approach in order to reach REFACTORING PHASE fast

--- src/Kata/FizzBuzz.php
+++ src/Kata/FizzBuzz.php
@@ -8,7 +8,7 @@ class FizzBuzz
     {
         if ($number % 3 === 0){
             return "Fizz";
-        } else if ($number === 5 || $number === 10){
+        } else if ($number === 5 || $number === 10 || $number === 20){
             return "Buzz";
         }
         return (string) $number;
