0f3ae92efe79590b39097d0fbb4683567ee71529
danielescillia <daniele.scillia@gmail.com>
2020-11-01 22:54:44 +0100

    added failing test to verify 2 return "2" as string - TRIANGULATION on same behaviour

--- tests/Kata/FizzBuzzTest.php
+++ tests/Kata/FizzBuzzTest.php
@@ -19,4 +19,10 @@ class FizzBuzzTest extends TestCase
         $actual = $this->fizzBuzz->handle(1);
         $this->assertSame("1", $actual);
     }
+
+    public function testItReturnsString2WhenReceivingNumber2(): void
+    {
+        $actual = $this->fizzBuzz->handle(2);
+        $this->assertSame("2", $actual);
+    }
 }
