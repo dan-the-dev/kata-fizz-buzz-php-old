fda3bcd7c4d388d4029da395bd2d9aeb82e0218a
danielescillia <daniele.scillia@gmail.com>
2020-11-01 23:16:01 +0100

    solved testItReturnsBuzzWhenReceivingNumber5 with OBVIOUS IMPLEMENTATION approach

--- src/Kata/FizzBuzz.php
+++ src/Kata/FizzBuzz.php
@@ -8,6 +8,8 @@ class FizzBuzz
     {
         if ($number % 3 === 0){
             return "Fizz";
+        } else if ($number === 5){
+            return "Buzz";
         }
         return (string) $number;
     }
