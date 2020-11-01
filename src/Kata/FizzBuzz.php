734aed57a64e32f76c1533f6bfa77767fe0db0cb
danielescillia <daniele.scillia@gmail.com>
2020-11-01 22:58:21 +0100

    REFACTOR in order to improve code and better solve the behaviour: return numbers as string

--- src/Kata/FizzBuzz.php
+++ src/Kata/FizzBuzz.php
@@ -6,12 +6,6 @@ class FizzBuzz
 {
     public function handle(int $number): string
     {
-        if ($number === 1){
-            return "1";
-        } else if ($number === 2){
-            return "2";
-        } else {
-            return "4";
-        }
+        return (string) $number;
     }
 }
