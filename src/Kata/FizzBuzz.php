35f436d9d7f8d96c1f73f43dfee0e65caaf5e963
danielescillia <daniele.scillia@gmail.com>
2020-11-01 23:10:36 +0100

    solved testItReturnsFizzWhenReceivingNumber6 with OBVIOUS IMPLEMENTATION approach

--- src/Kata/FizzBuzz.php
+++ src/Kata/FizzBuzz.php
@@ -6,7 +6,7 @@ class FizzBuzz
 {
     public function handle(int $number): string
     {
-        if ($number === 3) {
+        if ($number === 3 || $number === 6){
             return "Fizz";
         }
         return (string) $number;
