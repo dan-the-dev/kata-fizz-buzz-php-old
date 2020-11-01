d8363925aa034ee52704a7b589aa332ec4d17306
danielescillia <daniele.scillia@gmail.com>
2020-11-01 23:40:23 +0100

    finishing the kata with refactoring: incapsulated some logic and improved readibility

--- src/Kata/FizzBuzz.php
+++ src/Kata/FizzBuzz.php
@@ -4,15 +4,52 @@ namespace Kata;
 
 class FizzBuzz
 {
+    const FIZZ = "Fizz";
+    const BUZZ = "Buzz";
+    const FIZZ_BUZZ = "FizzBuzz";
+
     public function handle(int $number): string
     {
-        if ($number % 3 === 0 && $number % 5 === 0){
-            return "FizzBuzz";
-        } else if ($number % 3 === 0){
-            return "Fizz";
-        } else if ($number % 5 === 0){
-            return "Buzz";
+        $result = (string)$number;
+        if ($this->isDividableByBoth3And5($number)){
+            $result = self::FIZZ_BUZZ;
+        } else if ($this->isDividableBy3($number)){
+            $result = self::FIZZ;
+        } else if ($this->isDividableBy5($number)){
+            $result = self::BUZZ;
         }
-        return (string) $number;
+        return $result;
+    }
+
+    /**
+     * @param int $number
+     * @return bool
+     */
+    private function isDividableBy3(int $number): bool
+    {
+        return $this->isDividableBy($number, 3);
+    }
+
+    /**
+     * @param int $number
+     * @return bool
+     */
+    private function isDividableBy5(int $number): bool
+    {
+        return $this->isDividableBy($number, 5);
+    }
+
+    /**
+     * @param int $number
+     * @return bool
+     */
+    private function isDividableByBoth3And5(int $number): bool
+    {
+        return $this->isDividableBy3($number) && $this->isDividableBy5($number);
+    }
+
+    private function isDividableBy(int $number, int $divider): bool
+    {
+        return $number % $divider === 0;
     }
 }
