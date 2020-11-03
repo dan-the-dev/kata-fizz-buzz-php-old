3297ea1b044b159958c3c8885ba141e3f682e482
danielescillia <daniele.scillia@gmail.com>
2020-11-03 11:47:51 +0100

    refactor reached on 2nd behaviour: return "Fizz" when number is dividable by 3; written assertions before logic of test, decided test name at the end

--- src/Kata/FizzBuzz.php
+++ src/Kata/FizzBuzz.php
@@ -6,6 +6,9 @@ class FizzBuzz
 {
     public function handle(int $number): string
     {
+        if ($number % 3 === 0){
+            return "Fizz";
+        }
         return "$number";
     }
 }
--- tests/Kata/FuzzBuzzTest.php
+++ tests/Kata/FuzzBuzzTest.php
@@ -34,4 +34,60 @@ class FuzzBuzzTest extends TestCase
 
         $this->assertSame("4", $expected);
     }
+
+    public function testReturnsFizzWhenInputIs3(): void
+    {
+        $expected = $this->fizzBuzz->handle(3);
+
+        $this->assertSame("Fizz", $expected);
+    }
+
+    public function testReturnsFizzWhenInputIs6(): void
+    {
+        $expected = $this->fizzBuzz->handle(6);
+
+        $this->assertSame("Fizz", $expected);
+    }
+
+    public function testReturnsFizzWhenInputIs9(): void
+    {
+        $expected = $this->fizzBuzz->handle(9);
+
+        $this->assertSame("Fizz", $expected);
+    }
+
+    public function testReturnsFizzWhenInputIs12(): void
+    {
+        $expected = $this->fizzBuzz->handle(12);
+
+        $this->assertSame("Fizz", $expected);
+    }
+
+    public function testReturnsFizzWhenInputIs18(): void
+    {
+        $expected = $this->fizzBuzz->handle(18);
+
+        $this->assertSame("Fizz", $expected);
+    }
+
+    public function testReturnsFizzWhenInputIs99(): void
+    {
+        $expected = $this->fizzBuzz->handle(99);
+
+        $this->assertSame("Fizz", $expected);
+    }
+
+    public function testReturnsFizzWhenInputIs96(): void
+    {
+        $expected = $this->fizzBuzz->handle(96);
+
+        $this->assertSame("Fizz", $expected);
+    }
+
+    public function testReturnsFizzWhenInputIs93(): void
+    {
+        $expected = $this->fizzBuzz->handle(93);
+
+        $this->assertSame("Fizz", $expected);
+    }
 }
