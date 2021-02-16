67d9dcaf6d29d68f8477d463b83e4d3c940b9ad9
danielescillia <daniele.scillia@gmail.com>
2021-02-16 22:00:44 +0100

    tested first behavior: receive and int returns a string

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 # FIZZ BUZZ
 Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
+
+1 => "1"
+2 => "2"
+3 => "Fizz"
+4 => "4"
+5 => "Buzz"
+6 => "6"
+....
+15 => "FizzBuzz"
--- tests/Kata/FizzBuzzTest.php
+++ tests/Kata/FizzBuzzTest.php
@@ -14,13 +14,25 @@ class FizzBuzzTest extends TestCase
         $this->fizzBuzz = new FizzBuzz();
     }
 
-    public function testShallPass(): void
+    public function testNumber1ReturnsString1(): void
     {
-        $this->assertEquals(1, 1);
+        $actual = $this->fizzBuzz->handle(1);
+        $expected = "1";
+        $this->assertSame($expected, $actual);
     }
 
-    public function testHandleReturnTrue(): void
+    public function testNumber2ReturnsString2(): void
     {
-        $this->assertEquals(true, $this->fizzBuzz->handle());
+        $actual = $this->fizzBuzz->handle(2);
+        $expected = "2";
+        $this->assertSame($expected, $actual);
     }
+
+    public function testNumber4ReturnsString4(): void
+    {
+        $actual = $this->fizzBuzz->handle(4);
+        $expected = "4";
+        $this->assertSame($expected, $actual);
+    }
+    
 }
