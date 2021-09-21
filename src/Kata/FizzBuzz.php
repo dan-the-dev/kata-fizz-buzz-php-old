b1a7b2eaf3fee3896b2e891d2403806cc48d259a
danielescillia <daniele.scillia@gmail.com>
2021-09-21 10:03:19 +0200

    chore: reset test and code

--- src/Kata/FizzBuzz.php
+++ src/Kata/FizzBuzz.php
@@ -4,8 +4,5 @@ namespace Kata;
 
 class FizzBuzz
 {
-    public function handle(): bool
-    {
-        return true;
-    }
+
 }
--- tests/Kata/FizzBuzzTest.php
+++ tests/Kata/FizzBuzzTest.php
@@ -14,13 +14,5 @@ class FizzBuzzTest extends TestCase
         $this->fizzBuzz = new FizzBuzz();
     }
 
-    public function testShallPass(): void
-    {
-        $this->assertEquals(1, 1);
-    }
 
-    public function testHandleReturnTrue(): void
-    {
-        $this->assertEquals(true, $this->fizzBuzz->handle());
-    }
 }
