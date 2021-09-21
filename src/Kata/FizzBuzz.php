522c4fb80041de0078fd851027e2c2e83362c0b6
danielescillia <daniele.scillia@gmail.com>
2021-09-21 12:00:03 +0200

    chore: used "Obvious Implementation" strategy to make the second test green

--- src/Kata/FizzBuzz.php
+++ src/Kata/FizzBuzz.php
@@ -5,8 +5,11 @@ namespace Kata;
 class FizzBuzz
 {
 
-    public function printNumber()
+    public function printNumber(int $number)
     {
+        if ($number === 2) {
+            return 2;
+        }
         return 1;
     }
 }
