85bffa6aed7d5b4195d03a883f5c56d03a68c41c
danielescillia <daniele.scillia@gmail.com>
2021-09-21 12:11:45 +0200

    refactoring: 8. make the code return a string according to new test; still need to wrap the primitive string with a dedicated class

--- src/Kata/FizzBuzz.php
+++ src/Kata/FizzBuzz.php
@@ -5,11 +5,11 @@ namespace Kata;
 class FizzBuzz
 {
 
-    public function printNumber(Number $newFormatNumber)
+    public function printNumber(Number $newFormatNumber): string
     {
         if ($newFormatNumber->equals(new Number(2))) {
-            return 2;
+            return "2";
         }
-        return 1;
+        return "1";
     }
 }
