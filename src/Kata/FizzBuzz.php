ba0e2d8f635f425e65821a022f6066bbb4a1ef2e
dan-the-dev <daniele.scillia@gmail.com>
2022-03-18 11:23:52 +0100

    refactor: improved desgn of convert number method

--- src/Kata/FizzBuzz.php
+++ src/Kata/FizzBuzz.php
@@ -6,13 +6,7 @@ class FizzBuzz
 {
     public function convertNumber(int $number): string
     {
-        if ($number === 2) {
-            return '2';
-        }
-        if ($number === 4) {
-            return '4';
-        }
-        return '1';
+        return (string) $number;
     }
 
 
